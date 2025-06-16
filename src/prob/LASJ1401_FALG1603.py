53f2d9135d886981da4ce987617ac05dd08671c7
Justin Lasnier-Guillemette <Justin.Lasnier-Guillemette@krag.io>
2025-06-16 11:45:21 -0400

    1.3 fonctionnel

--- src/prob/LASJ1401_FALG1603.py
+++ src/prob/LASJ1401_FALG1603.py
@@ -242,21 +242,18 @@ class CustomDrillingController(robotcontrollers.RobotController):
         # Votre loi de commande ici !!!
         #################################
         
-        Fe = np.zeros((3,1))
-        Fe[3] = -200
+        Fe = np.array([0, 0, -200])
         
-        Kp = np.array([[50.0, 0.0, 0.0],[0.0, 50.0, 0.0],[0.0, 0.0, 0.0]])
-        Kd = np.array([[25.0, 0.0, 0.0],[0.0, 25.0, 0.0],[0.0, 0.0, 0.0]])
-        #e = 
-        #fe = K@
-
+        Kp = np.array([[100.0, 0.0, 0.0],[0.0, 100.0, 0.0],[0.0, 0.0, 100.0]])
+        Kd = np.array([[25.0, 0.0, 0.0],[0.0, 25.0, 0.0],[0.0, 0.0, 25.0]])
         
         u = np.zeros(self.m)  # place-holder de bonne dimension
+        Jtranp = np.transpose(J)
 
         if(self.case == 1) :
             r_g = np.array([0.25, 0.25, 0.5]) # bouger robot au dessu du preperçage 
             e = r_g - r
-            u = ... 
+            u = Jtranp@(Kp@e + Kd@(-J@dq)) + g
             # robot pret pour descendre pour placer meche dans preperçage
             if(np.linalg.norm(e) < 0.01 ): #10mm d'erreur
                 self.case = 2
@@ -264,7 +261,7 @@ class CustomDrillingController(robotcontrollers.RobotController):
         if(self.case == 2) :
             r_g = np.array([0.25, 0.25, 0.4]) # bouger robot au preperçage 
             e = r_g - r
-            u = g + np.transpose(J)@(Kp@e + Kd@(-J@dq)) 
+            u = Jtranp@(Kp@e + Kd@(-J@dq)) + g
             # robot pret a percer
             if(np.linalg.norm(e) < 0.001 ): #1mm d'erreur
                 self.case = 3
@@ -272,7 +269,7 @@ class CustomDrillingController(robotcontrollers.RobotController):
         #if(self.case == 3) :
         #    r_g = np.array([0.25, 0.25, 0.2]) # perçage
         #    e = r_g - r
-        #    u = np.transpose(J) @ Fe + g
+        #    u = Jtranp @ Fe + g
         #    # robot fini perçage
         #    if(r[2] < 0.2 ): #Fini de percer
         #        self.case = 4
@@ -280,12 +277,20 @@ class CustomDrillingController(robotcontrollers.RobotController):
         if(self.case == 3) :
             r_g = np.array([0.25, 0.25, 0.2]) # perçage
             e = r_g - r
-            Kp = np.array([[50.0, 0.0, 0.0],[0.0, 50.0, 0.0],[0.0, 0.0, 0.0]])
-            Kd = np.array([[25.0, 0.0, 0.0],[0.0, 25.0, 0.0],[0.0, 0.0, 0.0]])
-            u = np.transpose(J)@Fe + g + np.transpose(J)@(Kp@e + Kd@(-J@dq))
+            Kp = np.array([[80.0, 0.0, 0.0],[0.0, 80.0, 0.0],[0.0, 0.0, 1.0]])
+            Kd = np.array([[25.0, 0.0, 0.0],[0.0, 25.0, 0.0],[0.0, 0.0, 1.0]])
+            u = Jtranp @Fe + g + Jtranp@(Kp@e + Kd@(-J@dq)) #
             # robot fini perçage
             if(r[2] < 0.2 ): #Fini de percer
                 self.case = 4
+
+        if(self.case == 4) :
+            r_g = np.array([0.25, 0.25, 0.2]) # perçage
+            e = r_g - r
+            Kp = np.array([[0.0, 0.0, 0.0],[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]])
+            Kd = np.array([[0.0, 0.0, 0.0],[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]])
+            u = np.transpose(J)@Fe + g + np.transpose(J)@(Kp@e + Kd@(-J@dq))
+            # robot fini perçage
 
         return u
 
